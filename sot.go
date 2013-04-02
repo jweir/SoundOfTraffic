fe3399565d88617fc2001357f8378960d95c7b02
J. Weir <john@famedriver.com>
2013-04-01 23:59:34 -0400

    polish the code

--- sot.go
+++ sot.go
@@ -12,42 +12,17 @@ import (
 	"net/http"
 )
 
-type addrHdr interface {
-	SrcAddr() string
-	DestAddr() string
-	Len() int
-}
-
-type Source struct {
-	es    es.EventSource
-	label string
-	url   string
-}
-
-// Maps the type (TCP, UDP, ARP, etc) to an event source
-type SourceList map[string]Source
-
 func main() {
 	device := flag.String("i", "", "network device")
 	port := flag.String("p", "8000", "http server port (default 8000)")
 
 	flag.Parse()
 
-	sources := make(SourceList)
-
-	defer sources.add("tcp").Close()
-	defer sources.add("udp").Close()
-
-	http.Handle("/sources", sources)
-	http.Handle("/", http.FileServer(http.Dir("./pub")))
-
 	if *device == "" {
 		flag.PrintDefaults()
 		printDevices()
 	} else {
-		go openDevice(device, port, sources)
-		log.Printf("Opening server at localhost:%s and listening for %s\n", *port, *device)
-		log.Fatal(http.ListenAndServe(":"+*port, nil))
+		startServer(device, port)
 	}
 }
 
@@ -64,45 +39,63 @@ func printDevices() {
 	}
 }
 
-// binds an event source to a packet type (ie TCP)
-// creates an http handler to allow subscription to the packet type
-func (sources SourceList) add(label string) es.EventSource {
-	e := es.New(nil)
-	c := Source{
-		e,
-		label,
-		urlFor(label),
-	}
-	http.Handle(urlFor(label), e)
-	sources[label] = c
-	return e
+type Source struct {
+	es    es.EventSource
+	label string
+	url   string
 }
 
-func urlFor(s string) string {
-	return "/pcap/" + s + "/"
+// Maps the type (TCP, UDP, ARP, etc) to an event source
+type SourceMap map[string]Source
+
+func startServer(device *string, port *string) {
+	sm := make(SourceMap)
+
+	defer sm.Add("tcp").Close()
+	defer sm.Add("udp").Close()
+
+	http.Handle(sm["tcp"].url, sm["tcp"].es)
+	http.Handle(sm["udp"].url, sm["udp"].es)
+
+	http.Handle("/sources", sm)
+	http.Handle("/", http.FileServer(http.Dir("./pub")))
+
+	go openDevice(device, port, sm)
+	log.Printf("Opening server at localhost:%s and listening for %s\n", *port, *device)
+	log.Fatal(http.ListenAndServe(":"+*port, nil))
+}
+
+// binds an event source to a packet type (ie TCP)
+// creates an http handler to allow subscription to the packet type
+func (sm SourceMap) Add(label string) es.EventSource {
+	c := Source{
+		es:    es.New(nil),
+		label: label,
+		url:   "/pcap/" + label + "/"}
+	sm[label] = c
+	return c.es
 }
 
 // display the available sources as json
-func (c SourceList) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (s SourceMap) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	o := make(map[string]string)
-	for k, _ := range c {
-		o[k] = c[k].url
+	for k, _ := range s {
+		o[k] = s[k].url
 	}
 	m, _ := json.Marshal(o)
 	fmt.Fprintf(w, "%s", m)
 }
 
-func openDevice(device *string, port *string, sources SourceList) {
+func openDevice(device *string, port *string, sm SourceMap) {
 	h, err := pcap.OpenLive(*device, 65535, true, 100)
+
 	if h == nil {
 		fmt.Printf("Failed to open %s : %s\n", *device, err)
 	} else {
-
 		h.SetFilter("not port " + *port)
 		for pkt := h.Next(); ; pkt = h.Next() {
 			if pkt != nil {
-				pkt.Decode()
-				sources.send(pkt)
+				sm.process(pkt)
 			}
 		}
 
@@ -110,22 +103,44 @@ func openDevice(device *string, port *string, sources SourceList) {
 	}
 }
 
-func (s SourceList) send(pkt *pcap.Packet) {
+type ports struct {
+	SrcPort  uint16
+	DestPort uint16
+}
+
+type addrHdr interface {
+	SrcAddr() string
+	DestAddr() string
+	Len() int
+}
+
+func (s SourceMap) process(pkt *pcap.Packet) {
+	pkt.Decode()
+
 	if len(pkt.Headers) >= 2 {
 
 		if addr, ok := pkt.Headers[0].(addrHdr); ok {
 			switch k := pkt.Headers[1].(type) {
 			case *pcap.Tcphdr:
-				log.Printf("tcp %d", int(k.SrcPort))
-				m := fmt.Sprintf("%s:%d %s:%d", addr.SrcAddr(), int(k.SrcPort), addr.DestAddr(), int(k.DestPort))
-				s["tcp"].es.SendMessage(m, "", "")
+				f := &ports{k.SrcPort, k.DestPort}
+				s["tcp"].send(&addr, f)
 			case *pcap.Udphdr:
-				log.Printf("udp")
-				m := fmt.Sprintf("%s:%d %s:%d", addr.SrcAddr(), int(k.SrcPort), addr.DestAddr(), int(k.DestPort))
-				s["udp"].es.SendMessage(m, "", "")
+				f := &ports{k.SrcPort, k.DestPort}
+				s["udp"].send(&addr, f)
 			default:
 				log.Printf("%T", k)
 			}
 		}
 	}
+}
+
+func (s Source) send(addr *addrHdr, port *ports) {
+	m := msg(*addr, port)
+	s.es.SendMessage(m, "", "")
+}
+
+// Create the string representing the packet
+// this is what gets sent to the browser
+func msg(addr addrHdr, k *ports) string {
+	return fmt.Sprintf("%s:%d %s:%d", addr.SrcAddr(), int(k.SrcPort), addr.DestAddr(), int(k.DestPort))
 }
